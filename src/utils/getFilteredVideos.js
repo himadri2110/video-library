0eae129e13580eacc16f9f4f4d791d961a1c91f8
himadri2110 <18it.himadri.shah@gmail.com>
2022-04-27 18:32:11 +0530

    delete filter file

--- src/utils/getFilteredVideos.js
+++
@@ -1,8 +0,0 @@
-const getFilteredVideos = (videos, filterText) => {
-  if (filterText === "All") {
-    return videos;
-  }
-  return [...videos].filter((video) => video.category === filterText);
-};
-
-export { getFilteredVideos };
